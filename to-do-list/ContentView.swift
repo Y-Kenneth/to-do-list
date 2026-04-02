54e255536ab82236a4d456aca35c4d1b80984bd2
Kenneth <liekennie@gmail.com>
2026-04-02 09:03:08 +0800

    Initial Commit

---
+++ to-do-list/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  to-do-list
+//
+//  Created by 30 on 2026/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to-do-list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to-do-list
+//
+//  Created by 30 on 2026/4/2.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
